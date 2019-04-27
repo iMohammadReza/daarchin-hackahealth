64ca4f8f770f4349d0b5d0abb0a63efb095b9068
MohammadReza <mr3000persia@yahoo.com>
2019-04-27 18:51:01 +0430

    type missing

--- src/models/Commit.js
+++ src/models/Commit.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 var timestamps = require('mongoose-timestamp')
 
 const CommitSchema = new Schema({
-    action : { type : Schema.Types.ObjectId, ref : 'Action',
+    action : { type : Schema.Types.ObjectId, ref : 'Action'},
     tip : { type : Schema.Types.ObjectId, ref : 'Tip'},
     user : { type : Schema.Types.ObjectId, ref : 'User', required : true},
     value : { type: Object, required: true}
