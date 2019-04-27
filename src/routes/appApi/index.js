2f9474e813d5e13f19e2a10aeefce7693a9dfa82
MohammadReza <mr3000persia@yahoo.com>
2019-04-27 17:09:58 +0430

    Added profile function

--- src/routes/appApi/index.js
+++ src/routes/appApi/index.js
@@ -7,6 +7,7 @@ const AppController = require(`${controller}/AppController`)
 
 router.post('/phone' , AppController.phone.bind(AppController))
 router.post('/verify' , AppController.verify.bind(AppController))
+router.post('/profile' , AppController.profile.bind(AppController))
 router.post('/ask' , AppController.ask.bind(AppController))
 router.post('/home' , AppController.home.bind(AppController))
 router.post('/commit' , AppController.commit.bind(AppController))
