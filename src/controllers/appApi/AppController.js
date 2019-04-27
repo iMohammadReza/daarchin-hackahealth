f305581e58f4fa94471387d21864ba381dfadecf
MohammadReza <mr3000persia@yahoo.com>
2019-04-27 15:46:20 +0430

    completing phone and verify function

--- src/controllers/appApi/AppController.js
+++ src/controllers/appApi/AppController.js
@@ -3,18 +3,112 @@ import path from 'path'
 
 module.exports = new class AppController extends Controller {
   phone = (req, res) => {
-     
+    req.checkBody('phone' , 'وارد کردن شماره همراه الزامیست').notEmpty();
+    this.escapeAndTrim(req , 'phone');
+    if(this.showValidationErrors(req, res))
+          return;
+
+    /*
+
+
+
+    HERE WE SHOULD PREVENT BRUTEFORCING
+
+
+
+    */
+
+    let totp = new OTPAuth.TOTP({
+      issuer: '',
+      label: '',
+      algorithm: 'SHA1',
+      digits: 4,
+      period: 300,
+      secret: OTPAuth.Secret.fromRaw(req.body.phone)
+    });
+    
+    let token = totp.generate();
+    var api = Kavenegar.KavenegarApi({
+        apikey: process.env.KAVENEGAR_API
+    });
+
+    api.Send({
+      message: "کد تایید شما" + token,
+      sender: "10004346",
+      receptor: req.body.phone
+    }, function(response, status) {
+        console.log(response, status)
+        res.json({success: true})
+    });
   }
 
   verify = (req, res) => {
+    req.checkBody('phone' , 'وارد کردن شماره همراه الزامیست').notEmpty();
+    req.checkBody('token' , 'وارد کردن توکن الزامیست').notEmpty();
+    this.escapeAndTrim(req , 'phone token');
+    if(this.showValidationErrors(req, res))
+          return;
 
+    let totp = new OTPAuth.TOTP({
+      issuer: '',
+      label: '',
+      algorithm: 'SHA1',
+      digits: 4,
+      period: 300,
+      secret: OTPAuth.Secret.fromRaw(req.body.phone)
+    });
+    
+    let delta = totp.validate({
+      token: req.body.token,
+      window: 10
+    });
+
+    if(delta==0){
+
+      this.model.User.findOne({ phone : req.body.phone } , async (err , user) => {
+        if(err) throw err;
+
+        let user_id
+        if(user == null) {
+          let newUser = new this.model.User({ phone: req.body.phone });
+
+          await newUser.save(err => {
+            if(err)
+              return res.json({error: err, success: false});
+          });
+          console.log(newUser)
+          user_id = newUser._id
+        } else {
+          user_id = user._id
+        }
+          
+        let token = jwt.sign({ user_id } , config.secret , {expiresIn :  '360d'});
+        res.json({success: true, token})
+      })
+    }
+    else
+      res.json({success: false})
   }
 
   ask = (req, res) => {
+      req.checkBody('id' , '').notEmpty();
+      req.checkBody('value' , '').notEmpty();
+      req.checkBody('fvalue' , '').notEmpty();
+
+      this.escapeAndTrim(req , 'id value fvalue');
+
+      if(this.showValidationErrors(req, res))
+          return;
 
   }
 
   home = async (req, res) => {
+    req.checkBody('token' , '').notEmpty();
+
+    this.escapeAndTrim(req , 'token');
+
+    if(this.showValidationErrors(req, res))
+        return;
   }
 
   commit = (req, res) => {
--- src/models/User.js
+++ src/models/User.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema
 var timestamps = require('mongoose-timestamp')
 
 const UserSchema = new Schema({
-    name : { type : String , required : true},
+    name : { type : String , default : ""},
     phone : { type: String, required : true},
-    phone2 : { type: String, required : true},
+    phone2 : { type: String, default : ""},
     score : { type: Number, default: 0},
     game : { type: Object, default: []},
-    cencer : {type: Boolean, required: true},
+    cencer : {type: Boolean, default: false},
     periodDay : { type: Number, default: -1},
-    age : { type: Number, required: true},
-    sex : { type: Number, required: true} //0: undefined, 1:female, 2:male
+    age : { type: Number, default: -1},
+    sex : { type: Number, default: 0} //0: undefined, 1:female, 2:male
 })
 
 UserSchema.plugin(timestamps)
