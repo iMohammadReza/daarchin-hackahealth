5b59d6ddb789d8f6841f3b69e7d627274c7b0e95
markrahimi <kohiar77@gmail.com>
2019-04-29 05:17:43 +0430

    fixed admin panel

--- src/controllers/adminApi/adminController.js
+++ src/controllers/adminApi/adminController.js
@@ -8,22 +8,39 @@ module.exports = new class AdminController extends Controller {
         res.render(path.join(__dirname,'../../view/index.pug'), {authen: false, message: false})
       }
     post= (req, res) => {
-
-        if(req.body.username==null || req.body.password==null || req.body.username=="" || req.body.password==""){
+        if(req.body.type=="login"){
+          if(req.body.username==null || req.body.password==null || req.body.username=="" || req.body.password==""){
             res.status(422).render(path.join(__dirname,'../../view/index.pug'), {authen: false, message: true, error:"نام کاربری و رمز عبور نمیتواند خالی باشند"})
           } 
           else if(!(req.body.username=="hackahealth" && req.body.password=="2019hh")){
             res.status(403).render(path.join(__dirname,'../../view/index.pug'), {authen: false, message: true, error:"عدم دسترسی"})
           }
            else {
-            this.model.User.find({}, (err, users) => {
+            this.model.Commit.find({}).populate('user').populate('action').populate('tip').exec((err, commits) => {
               if(err) {
                 res.render(path.join(__dirname,'../../view/index.pug'), {authen: false, message: true, error: err.errmsg})
-                process.exit(1);
               }
-              res.render(path.join(__dirname,'../../view/index.pug'), {authen: true, message: false, message2: false, Items})
+              res.render(path.join(__dirname,'../../view/index.pug'), {authen: true, message: false, message2: false, commits})
             })
           }
+        }
+        else if(req.body.type=="search"){
+          if(req.body.search==null||req.body.search==""){
+              this.model.Commit.find({}).populate('user').populate('action').populate('tip').exec((err, commits) => {
+                console.log(commits)
+              res.render(path.join(__dirname,'../../view/index.pug'), {authen: true, message: false, message2: false, commits})
+            })
+          } else{
+            this.model.Commit.find({}).populate('user').populate('action').populate('tip').exec((err, commits) => {
+              if(err) {
+                res.render(path.join(__dirname,'../../view/index.pug'), {authen: false, message: true, error: err.errmsg})
+                
+              }
+              res.render(path.join(__dirname,'../../view/index.pug'),{authen: true, message: false, message2: false, commits: commits.filter((item)=>item.user.phone==req.body.search)})
+            })
+          }
+        }
+        
         
         
     }
